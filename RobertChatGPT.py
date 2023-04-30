b11d811ea8ca96dfa835b96052d21bb6f343b9a9
Bagelpolice <131701484+Bagelpolice@users.noreply.github.com>
2023-04-30 13:55:14 -0400

    Update RobertChatGPT.py

--- RobertChatGPT.py
+++ RobertChatGPT.py
@@ -1,34 +1,60 @@
-
 import openai
 import pyttsx3
 import speech_recognition as sr
-
+import serial
+import time
 
 
 openai.api_key = "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"
+
 engine = pyttsx3.init()
+
 r = sr.Recognizer()
 mic = sr.Microphone(device_index=1)
 
+# Wait for serial to connect
+time.sleep(3)
+
+# Names for bot to use
 conversation = ""
 user_name = "You"
 bot_name = "Holly"
 
-while True:
+expressions = {
+    "happy": ["great", "awesome", "fantastic"],
+    "sad": ["terrible", "awful", "not good"]
+}
+
+user_input = ""
+
+# connecting to serial port to communicate to Arduino
+serial = serial.Serial('COM4')
+
+
+while user_input != "stop":
     print("Say Holly to begin the recording.....")
+    
+    # listening for voice
     with mic as source:
-        #print("\nlistening...")
         r.adjust_for_ambient_noise(source, duration=1.0)
         audio = r.listen(source)
-    #print("no longer listening.\n")
-
+        
     try:
         user_input = r.recognize_google(audio)
+        serial.write("l".encode())
     except:
+        serial.write("d".encode())
         continue
-
+    
+    if user_input == "Holly test":
+        serial.write("t".encode())
+        # s.write("t")
+        print("sending serial test")
+        
+    
+    
+    # printing what the user said
     prompt = user_name + ": " + user_input + "\n" + bot_name+ ": "
-
     conversation += prompt  # allows for context
     print(prompt)
 
@@ -36,12 +62,25 @@ while True:
     response = openai.Completion.create(engine='text-davinci-003', prompt=conversation, max_tokens=100)
     response_str = response["choices"][0]["text"].replace("\n", "")
     response_str = response_str.split(user_name + ": ", 1)[0].split(bot_name + ": ", 1)[0]
-
+    
+    # printing Holly's reponse
     conversation += response_str + "\n"
     print(response_str)
 
-    
+    # determine appropriate expression based on response
+    expression = "neutral"
+    for exp, phrases in expressions.items():
+        if any(phrase in response_str for phrase in phrases):
+            expression = exp
+            break
+
+    # speak response through speaker
+    serial.write("t".encode());
     voice = engine.getProperty('voices')
-    engine.setProperty('voi..ce', voice[1].id)
+    engine.setProperty('voice', voice[1].id)
     engine.say(response_str)
     engine.runAndWait()
+    
+    # send expression to Arduino over serial
+    # ser.write(expression.encode())
+    
